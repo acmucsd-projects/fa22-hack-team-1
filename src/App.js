6e7383d17b4a8b8089be11df5e105511dedb8f6f
EvanLiu0 <ealiu@ucsd.edu>
2022-11-01 11:38:28 -0700

    Addded Prototype Home Page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div class="flex-main">
+        <div class="flex-left">
+          <img
+            class="image-main"
+            src={require("./images/rec.png")}
+            alt="You'll Just Have to Imagine the Fire"
+          />
+        </div>
+        <div class="flex-right">
+          <h1 class="right-header"> Welcome to UCSD Gym Bros</h1>
+          <form>
+            <label class="form-header" for="username">
+              Username:
+            </label>
+            <br />
+            <input
+              class="form-input"
+              type="text"
+              id="username"
+              name="username"
+            />
+            <br />
+            <br />
+            <label class="form-header" for="password">
+              Password:
+            </label>
+            <br />
+            <input
+              class="form-input"
+              type="password"
+              id="password"
+              name="password"
+            />
+            <br />
+            <br />
+            <br />
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
