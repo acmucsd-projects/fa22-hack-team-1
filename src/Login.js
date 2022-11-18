92a5e3bc6d29dbad4478f5fb9dc6be311e6747fd
bchoUCSD <bcho@ucsd.edu>
2022-11-17 18:18:43 -0800

    Create user page and CSS

--- src/Login.js
+++ src/Login.js
@@ -55,7 +55,7 @@ class Login extends Component {
             />
           </form>
         </div>
-      </div>
+      </div> 
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Login from "./Login";
+import CreateUser from "./CreateUser";
+import "./CreateUser.css"
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Login />
+    <CreateUser />
   </React.StrictMode>
 );
 
